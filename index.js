0a9fc0adaa0dae7de302dd6a22f83ccbb5b00967
matt <mattlim248@gmail.com>
2024-05-12 15:39:54 +0800

    commit changes

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express()
 const cors = require("cors")
+const https = require("https");
 
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 const fs = require("fs")
-const file = fs.readFileSync('./AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt')
+
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
+
+const cred = {
+  key,
+  cert,
+};
+
+const httpsServer = https.createServer(cred, app);
+// const file = fs.readFileSync('./AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt')
 
 
 const Authentication = require("./routers/authentication")
@@ -23,10 +34,14 @@ const expenses = require("./routers/expenseFetcher")
 
 app.use("/mb", Authentication, accChecker, budgeting, budgetFetcher, categories, expenses)
 
-app.get('/.well-known/pki-validation/AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt', (req, res) => {
-  res.sendFile('/home/ubuntu/budgeting-be/AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt')
-})
+// app.get('/.well-known/pki-validation/AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt', (req, res) => {
+//   res.sendFile('/home/ubuntu/budgeting-be/AAD326B5F8D3D6B26DF4A5BE3F5FA60A.txt')
+// })
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on port", process.env.PORT)
 })
+
+httpsServer.listen(process.env.HTTPSPORT, () => {
+    console.log(`HTTPS server is running on ${process.env.HTTPSPORT}`);
+  });
